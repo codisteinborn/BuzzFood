ab11aa2b120243abedd28320bc529fcf63e5e86a
Codi Steinborn <codisteinborn@Codis-MacBook-Pro.local>
2018-02-15 21:44:55 -0600

    files set up, questions written, results stored

---
+++ routes/apiRoutes.js
@@ -0,0 +1,45 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// ===============================================================================
+
+var resultData = require("../data/resultData");
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/results", function(req, res) {
+    res.json(resultData);
+  });
+
+  // API POST Requests
+  // Below code handles when a user submits a form and thus submits data to the server.
+  // In each of the below cases, when a user submits form data (a JSON object)
+  // ...the JSON is pushed to the appropriate JavaScript array
+  // (ex. User fills out a reservation request... this data is then sent to the server...
+  // Then the server saves the data to the tableData array)
+  // ---------------------------------------------------------------------------
+  app.post("/api/results", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+
+    
+    // figure out how to match surveyData to user's inputs
+    if (resultData.length < 5) {
+      resultData.push(req.body);
+      res.json(true);
+    }
+    else {
+      resultData.push(req.body);
+      res.json(false);
+    }
+  });
+
+};
