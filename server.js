6dbdf62dadc09b6e6dbe8b561d5ecfbfc9a65268
Codi Steinborn <codisteinborn@Codis-MacBook-Pro.local>
2018-02-19 16:00:24 -0600

    reorganization and pages link to one another

--- server.js
+++ server.js
@@ -21,8 +21,12 @@ app.use(bodyParser.json());
 // The below points our server to a series of "route" files.
 // These routes give our server a "map" of how to respond when users visit or request data from various URLs.
 // ================================================================================
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routes/apiRoutes")(app);
+require("./app/routes/htmlRoutes")(app);
+
+// Static directory
+app.use(express.static("app/public"));
+
 // =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
